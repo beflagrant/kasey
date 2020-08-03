28e6385d48864c8f98427ec9ad53f21837c7bbe4
Ben Vandgrift <ben@vandgrift.com>
2020-08-03 14:55:57 -0500

    maybe with a working_directory?

--- spec/app/models/smoke_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'smoke test' do
-  it { expect(true).to be_truthy }
-end
--- spec/support/factory_bot.rb
+++
@@ -1,5 +0,0 @@
-require 'factory_bot' or raise 'NO FACTORY BOT FOUND!'
-
-RSpec.configure do |config|
-    config.include FactoryBot::Syntax::Methods
-end
