39d74311269a3072dbe35c80fddda9a0a0ded2ab
Ben Vandgrift <ben@vandgrift.com>
2020-08-25 14:01:44 -0400

    cleans up `kase_managed` helper

--- lib/kasey/kase_managed.rb
+++ lib/kasey/kase_managed.rb
@@ -3,20 +3,12 @@ module Kasey
   module KaseManaged
     extend ActiveSupport::Concern
 
-    included do
-      def ensure_kase
-        Kasey::Kase.find_or_create_by(intake_id: id, intake_type: self.class.to_s)
-      end
-    end
-
     module ClassMethods
       def kase_managed
-        puts "MISCHIEF MANAGED"
-
         has_one :kase, class_name: "Kasey::Kase", as: :intake, dependent: :destroy
 
         after_create do
-          Kasey::Kase.create(intake: self)
+          Kasey::Kase.find_or_create_by(intake_id: id, intake_type: self.class.to_s)
         end
       end
     end
